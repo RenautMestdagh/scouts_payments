6a1a525ed78c4fe490167fbf909e4918cdb3047c
RenautMestdagh <renaut.mestdagh@hotmail.com>
2023-06-26 12:40:49 +0200

    favicon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const session = require('cookie-session');
+const favicon = require("serve-favicon");
 require('dotenv').config();
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico'))); //tab small picture (favicon)
 
 app.use(
     session({
